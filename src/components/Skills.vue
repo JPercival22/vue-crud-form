1246ba0d90b027faef7f8adb8ecd5cb5b85189be
Joseph Percival <joseph.m.percivaL@gmail.com>
2021-04-13 07:04:36 +0100

    added the UI, Interpolation skills form the logic into the template and styled.

---
+++ src/components/Skills.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="skills">
+    <div class="holder">
+
+      <input type="text" placeholder="Enter a Skill that you have..." v-model="skill">
+      {{ skill }}
+
+      <ul>
+        <li v-for="(data, index) in skills" :key='index'>{{index}}. {{data.skills}}</li>
+      </ul>
+      <p>These are the skills that you possess</p>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      skill: '',
+      skills: [
+        {"skills": "UX Researcher/ Designer"},
+        {"skills": "UI designer"},
+        {"skills": "Front End Developer"},
+        {"skills": "Vue.js"}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background: #323333;
+    color: #687f7f;
+  }
+
+</style>
