121695a0c33fdc6ef6cd6b19038ea210c8e15656
Joseph Percival <joseph.m.percivaL@gmail.com>
2021-04-13 09:05:15 +0100

    router implemented and functioning

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
-    <Skills />
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <router-view />
   </div>
 </template>
 
 <script>
-import Skills from './components/Skills.vue'
 
-export default {
-  name: 'App',
-  components: {
-    Skills
-  }
-}
 </script>
 
 
@@ -25,7 +23,7 @@ body {
   display: grid;
   grid-template-rows: auto;
   justify-items: center;
-  align-items: center;
+  padding-top: 50px;
 }
 body, html {
   margin: 0;
@@ -33,5 +31,19 @@ body, html {
 }
 #app {
     width: 50%;
+}
+
+nav {
+  padding: 20px 20px 20px 0;
+}
+
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: 700;
+  margin-right: 15px;
 }
 </style>
---
+++ src/components/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>Hello Music Magpie</h1>
+    <p>This is a quick demonstration on form functionality and routing.</p>
+  </div>
+</template>
+
+
